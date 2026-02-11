37a51da3183572b3c42d3e3cc9c6e50d217cac67
NemoArtifex <mweakes@gmail.com>
2026-02-10 20:14:56 -0500

    Create API for Search Service 2/10/26

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,3 +1,4 @@
+// 2/10/26 from solution
 // db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
@@ -13,19 +14,11 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-    await client.connect()
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-    dbInstance = client.db(dbName)// note 'giftdb' assigned to dbName earlier
-
-    // Task 3: Return database instance
-    // {{insert code}}
-    return dbInstance
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
